8025613568cd7bcb4e73da7d1981545199e8ed26
Saulo Christian <saullochristian@outlook.com>
2024-05-01 10:09:08 -0300

    Finalizando Details

--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from './styles'
+import { Container, Links, Content } from './styles'
 
 import { Tag } from '../../components/Tag'
 import { Button } from '../../components/Button'
@@ -10,22 +10,38 @@ export function Details() {
   return (
     <Container>
       <Header />
+      <main>
+        <Content>
+          <ButtonText title="Excluir Nota" />
 
-      <ButtonText title="Excluir Nota" />
+          <h1>Introdução ao React</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting 
+            industry. Lorem Ipsum has been the industry's standard dummy text 
+            ever since the 1500s, when an unknown printer took a galley of type 
+            and scrambled it to make a type specimen book. It has survived not 
+            only five centuries, but also the leap into electronic typesetting, 
+            remaining essentially unchanged. It was popularised in the 1960s with 
+            the release of Letraset sheets containing Lorem Ipsum passages, and 
+            more recently with desktop publishing software like Aldus PageMaker 
+            including versions of Lorem Ipsum.
+          </p>
 
-      <Section title="Links úteis">
-        <Links>
-          <li><a href="#">https://www.rocketseat.com.br/</a></li>
-          <li><a href="#">https://www.rocketseat.com.br/</a></li>
-        </Links>
-      </Section>
+          <Section title="Links úteis">
+            <Links>
+              <li><a href="#">https://www.rocketseat.com.br/</a></li>
+              <li><a href="#">https://www.rocketseat.com.br/</a></li>
+            </Links>
+          </Section>
 
-      <Section title="Marcadores">
-          <Tag title="Express" />
-          <Tag title="Node" />
-      </Section>
+          <Section title="Marcadores">
+              <Tag title="Express" />
+              <Tag title="Node" />
+          </Section>
 
-      <Button title="Voltar" />
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   )
 }
--- src/pages/details/styles.js
+++ src/pages/details/styles.js
@@ -9,6 +9,33 @@ export const Container = styled.div`
     grid-template-areas: 
     "header" 
     "content";
+
+    > main {
+        grid-area: content;
+        overflow-y: scroll;
+        padding: 64px 0;
+    }
+
+    /* Works on Firefox */
+    * {
+    scrollbar-width: thin;
+    scrollbar-color: ${({ theme }) => theme.COLORS.GRAY_300} ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+
+    /* Works on Chrome, Edge, and Safari */
+    *::-webkit-scrollbar {
+    width: 12px;
+    }
+
+    *::-webkit-scrollbar-track {
+    background: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+
+    *::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.COLORS.GRAY_300};
+    border-radius: 20px;
+    border: 3px solid ${({ theme }) => theme.COLORS.GRAY_100};
+    }
 `
 
 export const Links = styled.ul`
@@ -20,5 +47,29 @@ export const Links = styled.ul`
         a {
             color: ${({ theme }) => theme.COLORS.WHITE};
         }
+    }
+`
+
+export const Content = styled.div`
+    max-width: 550px;
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+
+    > button:first-child {
+        align-self: end;
+    }
+
+    > h1 {
+        font-size: 36px;
+        font-weight: 500;
+        padding-top: 64px;
+    }
+
+    > p {
+        font-size: 16px;
+        margin-top: 16px;
+        text-align: justify;  /* Alinha o texto de maneira centralizada */
     }
 `
