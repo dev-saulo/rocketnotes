a660880e42b5466379c47d5bdfb4e375d8deada9
Saulo Christian <saullochristian@outlook.com>
2024-05-01 09:05:39 -0300

    Ajustanto a imagem

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,18 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header() {
     return(
         <Container>
+            <Profile>
+                <img src="https://github.com/dev-saulo.png" 
+                alt="Foto do usuário"
+                />
+
+                <div>
+                   <span>Bem Vindo,</span>
+                   <strong>Saulo Christian</strong> 
+                </div>
+            </Profile>
 
         </Container>
     )
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -14,7 +14,33 @@ export const Container = styled.header`
     justify-content: space-between;
 
     padding: 0 80px;
+`;
 
+export const Profile = styled.div`
+    display: flex;
+    align-items: center;
 
-    background: red;
+    > img { /*O icone de > garante que será estilizado apenas a img (nesse caso) */
+        width: 56px;
+        height: 56px;
+        border-radius: 50%;
+    }
+
+    > div {
+        display: flex;
+        flex-direction: column;
+        margin-left: 16px;
+        line-height: 24px;
+
+        span {
+            font-size: 14px;
+            color: ${({ theme }) => theme.COLORS.GRAY_100};
+        }
+
+        strong {
+            font-size: 18px;
+            color: ${({ theme }) => theme.COLORS.WHITE};
+        }
+    }
+
 `;
